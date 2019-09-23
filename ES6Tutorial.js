85ccd455d8634fb981b588a587a9a6c300d0575d
sandeep0291 <kumar.sk05@gmail.com>
2019-09-23 11:52:56 +0530

    Spread Operator ||  added some comments for more understanding

--- ES6Tutorial.js
+++ ES6Tutorial.js
@@ -1,41 +1,37 @@
-// spread operator uses
+// SPREAD OPERATOR USES
+
+// 1.
+// spread operator very easy to uses to make object/array copy so that it doesn't mutate orignal object
+// check I have updated lastName and it doesn't mutate orignal object i.e person
 
 var person = { name: 'Sandeep', lastName: 'Kumar', address: 'Pune', contactNumber: 789292822 };
-
-// spread operator very easy to uses to make object copy so that it doesn't mutate orignal object
-// check I have updated lastName and 
 var personCopy = {...person, lastName: 'Arya'};
 
 console.log(personCopy);
-/*
-    {name: "Sandeep", lastName: "Arya", address: "Pune", contactNumber: 789292822}
-  
-*/
+/*  {name: "Sandeep", lastName: "Arya", address: "Pune", contactNumber: 789292822} */
 
 // Here lastName doesn't mutate/change
 console.log(person);
-/*
-   {name: "Sandeep", lastName: "Kumar", address: "Pune", contactNumber: 789292822}
-  
-*/
+/* {name: "Sandeep", lastName: "Kumar", address: "Pune", contactNumber: 789292822} */
+
 
 // Same scenario goes for an array
 
 var animal = [10,20,30,40,70];
-var animalCopy = [...animal];
+
+// pushed 89 here with copying from animal array to animalCopy
+var animalCopy = [...animal, 89];
+
+// normal way of pushing 
 animalCopy.push(80);
-// elements can be push inside array like this also
-//var animalCopy = [...animal, 100];
 
 console.log(animalCopy);
-/*
-[10, 20, 30, 40, 70, 80]
-*/
+//  [10, 20, 30, 40, 70, 80]
 
-// 
 console.log(animal);
-/*
-[10, 20, 30, 40, 70]
-*/
+// [10, 20, 30, 40, 70]
+
+
+
 
 
