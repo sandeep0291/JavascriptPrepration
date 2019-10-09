71ae25a3e9bf774c3f913e8283061de1377a69b4
sandeep0291 <kumar.sk05@gmail.com>
2019-10-09 16:11:31 +0530

    Added questions, will add answers later

---
+++ questionAndAnswer.js
@@ -0,0 +1,23 @@
+// Objects ***************
+// 1. How to clone objects
+// 2. how to deep clone objects
+// 3. How to merge objects
+// 4. What is object.assign()
+// 5. What is Object.keys and where it's useful
+
+
+
+// OOPS *********************
+// 1. What is __proto__
+// 2. Difference b/w prototype and __proto__
+// 3. What is a function constructor
+// 4. What is Object.create()
+// 5. Ways to achieve inheritance in javascript
+
+
+// Random *******************
+// 1. Currying
+// 2. Memonization
+// 3. Functional Programming
+// 4. Higher Order Functions
+// 5. Compose and pipe
