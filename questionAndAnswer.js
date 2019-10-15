d59ef1b80704cd14d06cd4a973ccac4827b483ae
sandeep0291 <kumar.sk05@gmail.com>
2019-10-15 21:25:18 +0530

    Answered || What is Object.keys and what does enumerable means

--- questionAndAnswer.js
+++ questionAndAnswer.js
@@ -4,6 +4,8 @@
 // 3. How to merge objects
 // 4. What is object.assign()
 // 5. What is Object.keys and where it's useful
+// 6. What is enumerable property means
+// 7. What is the difference b/w Object.assign and Object.create
 
 
 // ***********  Answer of 1. How to clone Objects *******************  @Sandeep Kumar
@@ -64,6 +66,37 @@ console.log(mergeThem); // {a: 10, b: 20}
 
 // This method is used to copy/merge objects as explained above, Please note this only does shallow copy as mentioned above
 // For more info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+
+
+// ***********  Answer of 5. What is Object.keys and where it's useful *******************  @Sandeep Kumar
+
+// Object.keys are used to iterate over object own enumerable property
+// above *own* means direct property and not like below
+var hello = { a: 1, b: { c: 2 }, d: 4 }
+console.log(Object.keys(hello));  // ["a", "b", "d"]
+// see above c will not iterate over, as it will iterate only over direct own property of an object
+Object.keys(hello).forEach(function(key){ 
+    console.log(hello[key]); // 1 // will give the value
+})
+// Object.keys() returns an array whose elements are strings corresponding to the enumerable properties found directly upon object. 
+//The ordering of the properties is the same as that given by looping over the properties of the object manually.
+
+
+
+// ***********  Answer of 7. What is enumerable property means *******************  @Sandeep Kumar
+
+// Enumerable basically a  property on an object, if it's set to false, for in and foreach loops will ignore that
+// Like if you check an an array it has property of length, but for in/ forEach loop ignores it
+// those property can been seen with Object.getOwnPropertyNames
+var arr = ['a', 'b', 'c'];
+console.log(Object.getOwnPropertyNames(arr).sort()); 
+// logs ["0", "1", "2", "length"] // and for-loop it's(length) ignored
+
+
+// how to set property to non-enmuerable, ex below
+var person = { age: 30 }
+Object.defineProperty(person, 'name', { value: 'Joshua', enumerable: false }); // now name will not be same when iterate in for loop/For Each
 
 
 
