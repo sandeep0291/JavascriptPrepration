2c477bd4df8d462e93635bcfeb185a3e31c08f2c
sandeep0291 <kumar.sk05@gmail.com>
2019-12-28 18:19:55 +0530

    singleton pattern

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -22,3 +22,51 @@ function duplicateString(str){
 }
 
 duplicateString('Sandeep');
+
+
+// Q2. What is Singleton Pattern
+
+// Singleton pattern in Javascript
+
+var Singleton = (function(){
+    var instance;
+    
+    var addition = function(a,b){
+        return a+b;
+    }
+
+     var subtraction = function(a,b){
+        return a-b;
+    }
+
+    function createInstance(){
+        return {
+            add: addition,
+            sub: subtraction
+        }
+    }
+
+    return {
+        getInstance: function(){
+            if(instance){
+                return instance;
+            }
+            else{
+                instance = createInstance();
+                return instance;
+            }
+        }
+    }
+   
+
+})();
+
+
+var instance1 = Singleton.getInstance();
+var instance2 = Singleton.getInstance();
+
+
+if(instance1 === instance2) 
+    console.log('Instance are same')
+else
+   console.log('Instance are not same')
