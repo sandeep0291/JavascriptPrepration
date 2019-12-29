a0ddd5fc1ac440f2b3b8fd560d66e41b4bb5cd82
sandeep0291 <kumar.sk05@gmail.com>
2019-12-29 18:25:37 +0530

    Flatten a nested array

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -70,3 +70,31 @@ if(instance1 === instance2)
     console.log('Instance are same')
 else
    console.log('Instance are not same')
+
+
+// Q3. Flatten a nested array
+// Flatten An an nested array 
+var  arr = [ [1,2,3,[4,5]] , [6,7], [8,9,[ 10, 11, [12,13] ]] ];
+
+
+function flattenArray(arr){
+   
+    //var i;
+    for( var i=0; i <arr.length; i++){
+
+        if( Array.isArray(arr[i]) ) {
+             var newArr = [].concat.apply([], arr[i]);
+             arr.splice(i,1);
+             arr = arr.concat(newArr);
+             i=-1;
+             console.log(arr);
+        }
+
+    }
+    return arr;
+}
+
+
+flattenArray(arr)
+
+
