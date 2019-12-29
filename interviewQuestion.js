8799bef0accb817b512a8400ea7df1ac71a24004
sandeep0291 <kumar.sk05@gmail.com>
2019-12-29 20:33:12 +0530

    Inheritance in Javascript

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -98,3 +98,39 @@ function flattenArray(arr){
 flattenArray(arr)
 
 
+// ***********  Q4. Inheritance in javascript *******************  @Sandeep Kumar
+function Animal (name, energy) {
+  this.name = name
+  this.energy = energy
+}
+
+Animal.prototype.eat = function (amount) {
+  console.log(`${this.name} is eating.`)
+  this.energy += amount
+}
+
+
+function Dog (name, energy, breed) {
+  Animal.call(this, name, energy); 
+    // .call() is a method on every function that allows you to invoke the function specifying 
+    // in what context the function will be invoked.
+    // here this is Dog context, now Dog will have Anima
+
+  this.breed = breed
+}
+
+// Now how we get the methods of Animal
+// By Mapping Dog.prototype to Aimal.prototype
+Dog.prototype = Object.create(Animal.prototype);
+// finally resetting constructor as we have overriden Dog.prototype.constructor function with Animal.protoype.constructot
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function () {
+  console.log('Woof Woof!')
+  this.energy -= .1
+}
+
+const charlie = new Dog('Charlie', 10, 'Goldendoodle')
+console.log(charlie.constructor)
+
+// Please refer to this link also for more details: https://tylermcginnis.com/javascript-inheritance-and-the-prototype-chain/
