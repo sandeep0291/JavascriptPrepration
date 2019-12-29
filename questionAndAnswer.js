e0b28352f014128af6144fd0c7f626d26b5b2e4c
sandeep0291 <kumar.sk05@gmail.com>
2019-12-29 20:32:24 +0530

    How to achieve Inheritance in javascript

--- questionAndAnswer.js
+++ questionAndAnswer.js
@@ -11,7 +11,7 @@
 // 1. What is __proto__
 // 2. Difference b/w prototype and __proto__
 // 3. What is a constructor function and factory function
-// 5. Ways to achieve inheritance in javascript
+// 5. Inheritance in javascript
 // 6. Singleton Pattern
 // 7. PubSub Pattern
 // 8. Modular Pattern
@@ -218,4 +218,45 @@ function helloWorld(){
 }
 var c = helloWorld();
 c.print();
+
+
+
+// ***********  Answer of  5. Inheritance in javascript *******************  @Sandeep Kumar
+function Animal (name, energy) {
+  this.name = name
+  this.energy = energy
+}
+
+Animal.prototype.eat = function (amount) {
+  console.log(`${this.name} is eating.`)
+  this.energy += amount
+}
+
+
+function Dog (name, energy, breed) {
+  Animal.call(this, name, energy); 
+    // .call() is a method on every function that allows you to invoke the function specifying 
+    // in what context the function will be invoked.
+    // here this is Dog context, now Dog will have Anima
+
+  this.breed = breed
+}
+
+// Now how we get the methods of Animal
+// By Mapping Dog.prototype to Aimal.prototype
+Dog.prototype = Object.create(Animal.prototype);
+// finally resetting constructor as we have overriden Dog.prototype.constructor function with Animal.protoype.constructot
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function () {
+  console.log('Woof Woof!')
+  this.energy -= .1
+}
+
+const charlie = new Dog('Charlie', 10, 'Goldendoodle')
+console.log(charlie.constructor)
+
+// Please refer to this link also for more details: https://tylermcginnis.com/javascript-inheritance-and-the-prototype-chain/
+
+
 
