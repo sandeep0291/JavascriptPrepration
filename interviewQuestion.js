c236d3cde18fecd8d2fef7395a9161932bf0b081
sandeep0291 <kumar.sk05@gmail.com>
2020-01-02 21:44:22 +0530

    Frequency Counter

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -217,3 +217,36 @@ removeItem(key) – remove the key with its value.
 clear() – delete everything.
 key(index) – get the key on a given position.
 length – the number of stored items.
+
+
+
+// Q56. Frequency pattern
+
+// Refactored code without nested loop
+function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for(let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+    }
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    return true
+}
+
+same([1,2,3,2,5], [9,1,4,4,11])
+
