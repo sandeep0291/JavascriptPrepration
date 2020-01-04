0ecbc6e6f4059465d652b8160bd4062e83f4b07e
sandeep0291 <kumar.sk05@gmail.com>
2020-01-04 19:31:23 +0530

    Weak map and difference b/w weak map and map

--- mapSets.js
+++ mapSets.js
@@ -18,6 +18,7 @@
 // you also have methods to clear a map or delete single items.
 
 // Map vs Object in JavaScript
+// Refer this link for more info: https://www.geeksforgeeks.org/map-vs-object-in-javascript/
 
 Map is a data structure which helps in storing the data in the form of pairs.
 The pair consists of a unique key and a value mapped to the key. It helps prevent duplicity.
@@ -30,3 +31,17 @@ Few basic differences are as follows:
 1. In Object, the data-type of the key-field is restricted to integer, strings, and symbols. Whereas in Map, the key-field can be of any data-type (integer, an array, even an object!)
 2. In the Map, the original order of elements is preserved. This is not true in case of objects.
 3. The Map is an instance of an object but the vice-versa is not true.
+
+
+// Weak Map
+
+// Weak map is almost similar to Map
+
+The main difference is that references to key objects in Map are strong while references to key objects in WeakMap are weak.
+i.e, A key object in WeakMap can be garbage collected if there is no other reference to it. Other differences are,
+
+1. Maps can store any key type Whereas WeakMaps can store only collections of key objects
+2. WeakMap does not have size property unlike Map
+3. WeakMap does not have methods such as clear, keys, values, entries, forEach.
+4. WeakMap is not iterable.
+
