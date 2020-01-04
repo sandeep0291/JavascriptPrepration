3566675523241400db29b2274db6535ad512e2f6
sandeep0291 <kumar.sk05@gmail.com>
2020-01-04 18:30:34 +0530

    Update myInterviewQuestion.js

--- myInterviewQuestion.js
+++ myInterviewQuestion.js
@@ -5,12 +5,28 @@ JavaScript Question
 
 Q1. What is difference b/w let,const,var and their scope.
 Q2. What is maps and weakmaps
+    // Refer here https://www.geeksforgeeks.org/map-vs-object-in-javascript/
 Q3. What is Event Propagation and difference between stopPropogation and ImmediateStopPropogation
 Q4. What is a Service Worker
 Q5. What is a Closure.
+    // Answered
 Q6. How to hide an object property
 Q7. What is the difference between application cache and browser cache.
-Q8. How to achieve deep copy and what if an object have functions in it
+    // HTML5 provides application cache, which means that a web application is cached, 
+    // and accessible without an internet connection. Application cache gives an application three advantages:
+    //   Offline browsing - users can use the application when they're offline
+    //   Speed - cached resources load faster Reduced server load - the
+    //   browser will only download updated/changed resources from the server Browser cache
+
+    //   Internet browsers use caching to store HTML web pages by storing a copy of visited pages and then using that copy to render when you re-visit that page. 
+    // . If the date on the page is the same date as the previously stored copy, then the computer uses the one on your hard drive rather than re-downloading it from the internet.
+    //   Internet browsers use caching to store HTML web pages by storing a copy of visited pages and then using that copy to render when you re-visit that page. 
+    //    If the date on the page is the same date as the previously stored copy, then the computer uses the one on your hard drive rather than re-downloading it from the internet.
+    //
+    // Refere here: https://stackoverflow.com/questions/22958006/browser-cache-vs-html5-application-cache
+Q8. How to achieve deep copy and what if an object have functions in it.
+    // For normal nested object without function,regex extra JSON.stringify
+    // else we need to use library like loadash or maybe jquery
 
 
 CSS Question
