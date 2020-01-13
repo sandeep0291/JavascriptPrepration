34e08db3f5df933c4c628141013fe17474623e98
Sandeep Kumar <kumar.sk05@gmail.com>
2020-01-13 11:27:22 +0530

    TapeEquilibrium

--- sortingSearchinAlgo.js
+++ sortingSearchinAlgo.js
@@ -2,4 +2,5 @@
 
 1. CyclicRotation         https://app.codility.com/demo/results/trainingCEZKNG-7NC/
 2. OddOccurrencesInArray  https://app.codility.com/demo/results/training8EUXY2-Q8Z/      
-3. 
+3. FrogRiverOne           https://app.codility.com/demo/results/trainingE5WM8C-MVY/
+4. TapeEquilibrium        https://app.codility.com/demo/results/trainingCJ44QG-373/
