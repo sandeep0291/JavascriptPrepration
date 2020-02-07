3aa043b10e1e21d0033acf910a88df7938650403
Sandeep Kumar <kumar.sk05@gmail.com>
2020-02-07 13:32:31 +0530

    BFS DFSPreOrder DFSPostOrder DFSInOrder

--- dataStructures/treeTraversals.js
+++ dataStructures/treeTraversals.js
@@ -65,9 +65,6 @@ class BinarySearchTree {
         }
         return false;
     }
-
-    // Breath First Search ***************
-    // **********************************
     BFS(){
         var node = this.root,
             data = [],
@@ -82,6 +79,36 @@ class BinarySearchTree {
         }
         return data;
     }
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSInOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 
@@ -92,6 +119,12 @@ tree.insert(15);
 tree.insert(3);
 tree.insert(8);
 tree.insert(20);
-tree.BFS();
+tree.DFSPreOrder();
+tree.DFSPostOrder();
+tree.DFSInOrder();
+
+
+
+
     
 
