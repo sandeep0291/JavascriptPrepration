84abbb133d6240ec00214f709bba7e84d23e1be2
Sandeep Kumar <kumar.sk05@gmail.com>
2020-12-09 00:53:34 +0530

    closure related questions

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -405,3 +405,29 @@ mergeSort(array);
   }
 })();
 
+
+// another example of similar fashion
+function showHelp(help) {
+  document.getElementById('help').innerHTML = help;
+}
+
+function makeHelpCallback(help) {
+  return function() {
+    showHelp(help);
+  };
+}
+
+function setupHelp() {
+  var helpText = [
+      {'id': 'email', 'help': 'Your e-mail address'},
+      {'id': 'name', 'help': 'Your full name'},
+      {'id': 'age', 'help': 'Your age (you must be over 16)'}
+    ];
+
+  for (var i = 0; i < helpText.length; i++) {
+    var item = helpText[i];
+    document.getElementById(item.id).onfocus = makeHelpCallback(item.help);
+  }
+}
+
+setupHelp();
